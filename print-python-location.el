ac25313790128e4ff906144f352b84238140c702
tlestang <thibault.lestang@meteo.fr>
2024-07-24 16:52:44 +0200

    Extract function

--- print-python-location.el
+++ print-python-location.el
@@ -2,16 +2,23 @@
   (string-match  mystr)
   (match-string 2 mystr))
 
-(defun tl-goto-def ()
-  (interactive)
+(defun tl-get-object-name (type)
   (save-excursion
     (search-backward-regexp
      (concat
       "\\("
       (make-string python-indent-offset ?\s)
-      "\\)*def"))
+      "\\)*"
+      type))
     (let ((line (buffer-substring-no-properties
 		 (line-beginning-position) (line-end-position)))
 	  (rgxp "\\(def\\|class\\) \\([_a-zA-Z0-9]*\\)\\((.*)\\)?:"))
       (string-match rgxp line)
-      (message (match-string 2 line)))))
+      (match-string 2 line))))
+
+(defun tl-goto-def ()
+  (interactive)
+  (let ((funcname (tl-get-object-name "def"))
+	(classname (tl-get-object-name "class")))
+    (message (concat
+	      classname "." funcname))))
